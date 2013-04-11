58805979e676c16a875929e5352bcd3d27044612
rmcfrazier <rmcfrazier@gmail.com>
2013-04-11 16:51:39 -0700

    Initial Commit

--- build/package.php
+++ build/package.php
@@ -16,10 +16,6 @@ define('LIB_PATH', BASE_PATH.DIRECTORY_SEPARATOR.'lib');
 //define('SRC_PATH', APP_PATH.DIRECTORY_SEPARATOR.'src');
 
 /*
- * Let the user know what is going on
- */
-//echo "Creating phar for php-impala located at ".$zfLocation."\n";
-/*
  * Clean up from previous 
  */
 if (file_exists('php-impala.phar')) {
